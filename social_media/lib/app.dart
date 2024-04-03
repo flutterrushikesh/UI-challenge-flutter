93b021872068eb97bcee2c3f001f2c6c70d32226
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-03 19:54:47 +0530

    working on toggle button

--- social_media/lib/app.dart
+++ social_media/lib/app.dart
@@ -7,11 +7,39 @@ class SocialMedia extends StatefulWidget {
 }
 
 class _SocialMediaState extends State {
+  bool _isSelected = false;
+
+  void _toggleSelection() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.more),
+        leading: const Icon(Icons.menu, color: Colors.white, size: 29),
+        backgroundColor: Colors.blue,
+        title: const Text("Social Media"),
+        actions: [
+          ToggleButtons(
+            isSelected: [_isSelected, !_isSelected],
+            onPressed: (int index) {
+              _toggleSelection();
+            },
+            children: const <Widget>[
+              Icon(Icons.check),
+              Icon(Icons.clear),
+            ],
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          _isSelected ? 'Selected' : 'Not Selected',
+          style: const TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
