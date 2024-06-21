278ab249eac73003ec907a4311a6396849e7f82e
flutterrushikesh <flutterrushikesh@gmail.com>
2024-06-21 17:29:31 +0530

    Bottom navigation bar remaining

--- pet_care/lib/screens/veterinary_screen.dart
+++ pet_care/lib/screens/veterinary_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:pet_care/my_widgets/adds_container.dart';
 import 'package:pet_care/my_widgets/my_searchbox.dart';
 import 'package:pet_care/my_widgets/mydoctorlist.dart';
 import 'package:pet_care/my_widgets/ourservicelist.dart';
+import 'package:pet_care/screens/doctor_detail_screen.dart';
 
 class VeterinaryScreen extends StatefulWidget {
   const VeterinaryScreen({super.key});
@@ -43,11 +43,29 @@ class _VeterinaryScreenState extends State<VeterinaryScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: ListView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const MyAddsContainer(
-              description: 'Lets Find Specialist \nDoctor for Your Pet!',
-              imagePath: 'assets/images/veterinaryadd.png',
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: const Color.fromRGBO(245, 146, 69, 1),
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    'Lets Find Specialist \nDoctor for Your Pet!',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  Image.asset('assets/images/veterinaryadd.png'),
+                ],
+              ),
             ),
             const SizedBox(height: 20),
             const MySearchBox(),
@@ -107,28 +125,43 @@ class _VeterinaryScreenState extends State<VeterinaryScreen> {
             const SizedBox(
               height: 10,
             ),
-            const DoctorList(
-              doctorImgpath: 'assets/images/veterinarydentist.png',
-              doctorLocationFrom: '1.0 km',
-              doctorName: 'Dr. Anna Johanson',
-              doctorRating: '4.8',
-              doctorType: 'Veterinary Behavioral',
-            ),
-            const SizedBox(height: 15),
-            const DoctorList(
-              doctorImgpath: 'assets/images/veterinarysurgery.png',
-              doctorLocationFrom: '1.5 km',
-              doctorName: 'Dr. Vernon Chwe',
-              doctorRating: '4.9',
-              doctorType: 'Veterinary Surgery',
-            ),
-            const SizedBox(height: 15),
-            const DoctorList(
-              doctorImgpath: 'assets/images/veterinarydentist2.png',
-              doctorLocationFrom: '2.6 km',
-              doctorName: 'Dr. Maria Nai',
-              doctorRating: '4.8',
-              doctorType: 'Veterinary Dentist',
+            Expanded(
+              child: ListView(
+                children: const [
+                  DoctorList(
+                    doctorImgpath: 'assets/images/veterinarydentist.png',
+                    doctorLocationFrom: '1.0 km',
+                    doctorName: 'Dr. Anna Johanson',
+                    doctorRating: '4.8',
+                    doctorType: 'Veterinary Behavioral',
+                    navigateTo: AnnaJohansonDetails(),
+                  ),
+                  SizedBox(height: 15),
+                  DoctorList(
+                    doctorImgpath: 'assets/images/veterinarysurgery.png',
+                    doctorLocationFrom: '1.5 km',
+                    doctorName: 'Dr. Vernon Chwe',
+                    doctorRating: '4.9',
+                    doctorType: 'Veterinary Surgery',
+                  ),
+                  SizedBox(height: 15),
+                  DoctorList(
+                    doctorImgpath: 'assets/images/veterinarydentist2.png',
+                    doctorLocationFrom: '2.6 km',
+                    doctorName: 'Dr. Maria Nai',
+                    doctorRating: '4.8',
+                    doctorType: 'Veterinary Dentist',
+                  ),
+                  SizedBox(height: 15),
+                  DoctorList(
+                    doctorImgpath: 'assets/images/veterinarysurgery.png',
+                    doctorLocationFrom: '1.5 km',
+                    doctorName: 'Dr. Vernon Chwe',
+                    doctorRating: '4.9',
+                    doctorType: 'Veterinary Surgery',
+                  ),
+                ],
+              ),
             ),
           ],
         ),
